c62245c216821ac8e60559c2af4691c68fbbddf1
WASHIDHUSSAIN825 <washid.hussain@parati.in>
2024-12-06 19:45:57 +0530

    logo fix

--- src/components/landing-page/LandingPageNavbar.tsx
+++ src/components/landing-page/LandingPageNavbar.tsx
@@ -28,7 +28,7 @@ const LandingPageNavbar: React.FC = () => {
         <div className="text-xl font-bold text-gray-800">
           <span className="flex items-center">
             <a
-              href="http://localhost:3000/"
+              href="/"
               target="_blank"
               rel="noopener noreferrer"
             >
