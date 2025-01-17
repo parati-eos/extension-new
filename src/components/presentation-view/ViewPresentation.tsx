eedc43abaf7f69b652afea4be1b1f9941536efe8
suvigya-mishra <suvigya.mishra@parati.in>
2025-01-17 18:53:40 +0530

    Testing Pricing Plans

--- src/components/presentation-view/ViewPresentation.tsx
+++ src/components/presentation-view/ViewPresentation.tsx
@@ -1439,16 +1439,20 @@ export default function ViewPresentation() {
           const country = ipInfoData!.country!
 
           if (country !== 'IN' && country !== 'India' && country !== 'In') {
-            setMonthlyPlan(response.data.items[4])
-            setYearlyPlan(response.data.items[2])
+            // setMonthlyPlan(response.data.items[4])
+            // setYearlyPlan(response.data.items[2])
+            setMonthlyPlan(response.data.items[1])
+            setYearlyPlan(response.data.items[0])
             setCurrency('USD')
           } else if (
             country === 'IN' ||
             country === 'India' ||
             country === 'In'
           ) {
-            setMonthlyPlan(response.data.items[5])
-            setYearlyPlan(response.data.items[3])
+            // setMonthlyPlan(response.data.items[5])
+            // setYearlyPlan(response.data.items[3])
+            setMonthlyPlan(response.data.items[1])
+            setYearlyPlan(response.data.items[0])
             setCurrency('INR')
           }
         })
