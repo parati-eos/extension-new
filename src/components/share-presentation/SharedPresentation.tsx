831b6ebc6df456c8c8f8de0d9eae83583c8506cc
suvigya-mishra <suvigya.mishra@parati.in>
2025-01-22 19:37:55 +0530

    Share Optimize

--- src/components/share-presentation/SharedPresentation.tsx
+++ src/components/share-presentation/SharedPresentation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Googleslides from './GoogleSlides.tsx'
 import ZynthLogo from '../../assets/zynth-text.png'
 import { GooglePresentationProps } from '../../types/types.ts'
@@ -13,53 +13,7 @@ const GooglePresentation: React.FC<GooglePresentationProps> = ({ formId }) => {
 }
 
 const SharedPresentation = ({ formId }: SharedPresentationProps) => {
-  const authToken = sessionStorage.getItem('authToken')
-  const [companyLogo, setCompanyLogo] = useState('')
-  const [companyLink, setCompanyLink] = useState('')
-
-  useEffect(() => {
-    const fetchSlidesData = async () => {
-      try {
-        const serverurl = process.env.REACT_APP_BACKEND_URL
-        const url = `${serverurl}/api/v1/data/appscripts/genSlideIDs/${formId}`
-        const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        })
-        if (!response.ok) {
-          throw new Error('Failed to fetch slides data')
-        }
-        const data = await response.json()
-        setCompanyLink(data.websiteLink)
-        setCompanyLogo(data.companyLogo)
-      } catch (error: any) {
-        console.error('Error fetching slides data:', error.message)
-      }
-    }
-    fetchSlidesData()
-  }, [formId, authToken])
-
-  return (
-    <div className="w-full h-screen no-scrollbar no-scrollbar::-webkit-scrollbar">
-      <div className="flex flex-col items-center justify-center py-2 bg-gray-50">
-        <a
-          href={companyLink}
-          target="_blank"
-          className="transition-all duration-300 transform hover:scale-110 active:scale-95 active:opacity-80"
-        >
-          <img
-            src={companyLogo}
-            alt="Parati Logo"
-            width={150}
-            className="text-center"
-          />
-        </a>
-      </div>
-      <GooglePresentation formId={formId} />
-    </div>
-  )
+  return <GooglePresentation formId={formId} />
 }
 
 export default SharedPresentation
