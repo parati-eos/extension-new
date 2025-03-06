5e7749a569d37b7748d9980c4292fd3e34486379
WASHIDHUSSAIN825 <washid.hussain@parati.in>
2025-03-06 10:31:48 +0530

    modal-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
